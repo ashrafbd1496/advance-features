64097e374f59256ebffbcd3c5233c7ba6b13a67d
ashrafbd1496 <asraf1496@gmail.com>
2023-12-18 09:34:38 +0600

    plugin header added

---
+++ advance-features.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name
+ *
+ * @package           PluginPackage
+ * @author            Your Name
+ * @copyright         2019 Your Name or Company Name
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Advance Features
+ * Plugin URI:        https://github.com/advance-features
+ * Description:       Plugin for extending theme feature
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Ashraf Uddin
+ * Author URI:        https://ashrafbd.com
+ * Text Domain:       adv-ftrs
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://github.com/advance-features/
+ */
